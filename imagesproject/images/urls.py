d53a0ea5ee01150e60727a2bd590078c5f0823fa
kulas971 <rehan.mateusz@gmail.com>
2021-09-16 12:53:50 +0200

    added ImageRetrieve view

--- imagesproject/images/urls.py
+++ imagesproject/images/urls.py
@@ -6,4 +6,5 @@ app_name = 'images'
 
 urlpatterns = [
     path('', views.ImageListCreateView.as_view()),
+    path('<int:pk>/', views.ImageRetriveAPIVIew.as_view()),
 ]
--- imagesproject/images/views.py
+++ imagesproject/images/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from . import serializers
 from . import models
@@ -12,3 +13,15 @@ class ImageListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        if self.request.user.plan.has_original:
+            queryset = queryset.filter(owner=self.request.user)
+            serializer = serializers.ImageSerializer(queryset, many=True,
+                context = self.get_serializer_context())
+        return Response(serializer.data)
+
+class ImageRetriveAPIVIew(generics.RetrieveAPIView):
+    serializer_class = serializers.ImageRetrieveSerializer
+    queryset = models.Image.objects.all()
