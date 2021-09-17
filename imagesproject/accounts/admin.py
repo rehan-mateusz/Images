8b5226b9ae9d23218cd09e6a7a9d4d2336d8a6c7
kulas971 <rehan.mateusz@gmail.com>
2021-09-17 12:34:30 +0200

    added feature - on user.plan update delete old thumbnails and create new

--- imagesproject/accounts/admin.py
+++ imagesproject/accounts/admin.py
@@ -1,9 +1,12 @@
+import json
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django import forms
 
 from . import models
-
+from images import models as img_models
+from images import images_utils
 
 class AccountCreationForm(forms.ModelForm):
     password1 = forms.CharField(label='Password')
@@ -25,7 +28,23 @@ class AccountChangeForm(forms.ModelForm):
 
     class Meta:
         model = models.Account
-        fields = '__all__'
+        fields = ('__all__')
+
+    def save(self, commit=True):
+        pre_update_account =  models.Account.objects.get(
+            id=self.instance.id)
+        if pre_update_account.plan != self.cleaned_data['plan']:
+            images = img_models.Image.objects.filter(owner=pre_update_account)
+            for image in images:
+                thumbnails=image.thumbnail_set.all()
+                for thumbnail in thumbnails:
+                    thumbnail.delete()
+                if self.cleaned_data['plan']:
+                    sizes = json.loads(self.cleaned_data['plan'].thumbnails_sizes['sizes'])
+                    images_utils.create_thumbnails(image, sizes)
+        updated_account = super().save(commit=False)
+        updated_account.save()
+        return updated_account
 
 class AccountUserAdmin(UserAdmin):
     form = AccountChangeForm
@@ -33,7 +52,7 @@ class AccountUserAdmin(UserAdmin):
     list_display = ('email', 'username', 'plan')
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password', )}),
         ('Plan', {'fields': ('plan',)}),
         ('Permissions', {'fields': ('is_admin','is_active', 'is_staff', 'is_superuser')}),
     )
