00cc3c9d7530b1dd0e5d25fb94d4b88e9abe7d0c
kulas971 <rehan.mateusz@gmail.com>
2021-09-23 12:27:45 +0200

    fixed createlistview - added permission for users without plan, removed has_object check from view

--- imagesproject/images/permissions.py
+++ imagesproject/images/permissions.py
@@ -1,10 +1,17 @@
 from rest_framework import permissions
 
 
+class HasPlan(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.plan is not None
+
+
 class IsImageOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, image):
         return image.owner == request.user
+
 
 class IsImageOwnerAndCanLink(permissions.BasePermission):
 
--- imagesproject/images/views.py
+++ imagesproject/images/views.py
@@ -19,7 +19,7 @@ from . import models
 
 class ImageListCreateView(generics.ListCreateAPIView):
     serializer_class = serializers.ImageSerializer
-    permission_classes = [IsAuthenticated,]
+    permission_classes = [(IsAuthenticated and permissions.HasPlan), ]
     queryset = models.Image.objects.all()
 
     def perform_create(self, serializer):
@@ -27,21 +27,19 @@ class ImageListCreateView(generics.ListCreateAPIView):
 
     def list(self, request):
         queryset = self.get_queryset()
-        if self.request.user.plan.has_original:
-            queryset = queryset.filter(owner=self.request.user)
-            serializer = serializers.ImageSerializer(queryset, many=True,
-                context = self.get_serializer_context())
+        serializer = serializers.ImageSerializer(
+            queryset, many=True, context=self.get_serializer_context())
         return Response(serializer.data)
 
 
 class ImageRetriveAPIView(generics.RetrieveAPIView):
-    permission_classes = [permissions.IsImageOwner,]
+    permission_classes = [permissions.IsImageOwner, ]
     serializer_class = serializers.ImageRetrieveSerializer
     queryset = models.Image.objects.all()
 
 
 class TempURLCreateView(generics.RetrieveAPIView):
-    permission_classes = [permissions.IsImageOwnerAndCanLink,]
+    permission_classes = [permissions.IsImageOwnerAndCanLink, ]
     serializer_class = serializers.ImageRetrieveSerializer
     queryset = models.Image.objects.all()
 
@@ -49,16 +47,17 @@ class TempURLCreateView(generics.RetrieveAPIView):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
         url = models.TempURL(
-            id = secrets.token_urlsafe(32),
-            data = json.dumps(dict(serializer.data)),
-            valid_until = datetime.now(timezone.utc) + timedelta(seconds=self.kwargs['seconds']))
+            id=secrets.token_urlsafe(32),
+            data=json.dumps(dict(serializer.data)),
+            valid_until=datetime.now(timezone.utc) + timedelta(
+                seconds=self.kwargs['seconds']))
         try:
             url.full_clean()
             url.save()
         except ValidationError:
             return Response('Link can be valid between 30 and 30000 seconds')
         return Response(self.request.build_absolute_uri(
-                    reverse('images:temp_url', kwargs={'pk': url.id})))
+            reverse('images:temp_url', kwargs={'pk': url.id})))
 
 
 class TempURLView(generics.RetrieveAPIView):
